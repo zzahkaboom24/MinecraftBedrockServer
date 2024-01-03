9b1dde71bc8cb26270cb35e2f1f812d473d06bcc
zzahkaboom24 <42496614+zzahkaboom24@users.noreply.github.com>
2024-01-03 06:21:06 +0100

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -127,8 +127,8 @@ if [ "$ViewManager" == "screen" ]; then
   fi
 elif [ "$ViewManager" == "tmux" ]; then
   if tmux list-sessions -F "#{session_name} #{window_name} (created #{session_created})" | awk -F " " '{printf "%s: %s (%s)\n", $1, $2, strftime("%Y-%m-%d %H:%M:%S", $4)}' | sed 's/ (created [0-9]*)//' | tr -s ' ' | grep -q "^MinecraftBedrockServer: servername"; then
-  echo "Minecraft server still hasn't stopped after 20 seconds, closing screen manually"
-  tmux kill-session -t MinecraftBedrockServer
+    echo "Minecraft server still hasn't stopped after 20 seconds, closing screen manually"
+    tmux kill-session -t MinecraftBedrockServer
   fi
 fi
 
