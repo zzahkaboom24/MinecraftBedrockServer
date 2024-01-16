e671691246d7643a118b8531a3333af3ece2a239
zzahkaboom24 <42496614+zzahkaboom24@users.noreply.github.com>
2024-01-16 15:27:41 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -17,4 +17,4 @@ if [ $(id -u) = 0 ]; then
    exit 1
 fi
 
-curl https://raw.githubusercontent.com/zzahkaboom24/MinecraftBedrockServer/master/SetupMinecraft.sh | bash
+curl -sSL https://raw.githubusercontent.com/zzahkaboom24/MinecraftBedrockServer/master/SetupMinecraft.sh | bash
